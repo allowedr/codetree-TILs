ff00d1c051ef1978b791789ce34dcf5aa9f9113d
allowedr <83686606+allowedr@users.noreply.github.com>
2024-09-24 10:53:50 +0900

    Title: 두 사람, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/두 사람/two-person.py
+++ 240924/두 사람/two-person.py
@@ -1,8 +1,7 @@
 a = input().split(' ')
 b = input().split(' ')
 
-
-if a[0] or b[0] > 19 and a[1] or b[1] > 'M' :
+if int(a[0]) > 19 and a[1] == 'M' or int(b[0]) > 19 and b[1] == 'M' :
     print('1')
 else :
     print('0')
