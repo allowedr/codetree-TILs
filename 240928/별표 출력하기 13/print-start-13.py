ad794c6f89c42188f64b100e8ed0536af35b93fe
allowedr <83686606+allowedr@users.noreply.github.com>
2024-09-28 17:35:21 +0900

    Title: 별표 출력하기 13, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240928/별표 출력하기 13/print-start-13.py
+++ 240928/별표 출력하기 13/print-start-13.py
@@ -1,15 +1,10 @@
 n = int(input())
-print("* "*n)
-for i in range(1, n) :
-    if i%2 ==0 :
-        print("* " * i)
-    else :
-        print("* " * i)
-        i += 1
-for j in  range(i) :
-    if i%2 !=0 :
-        print("* " * i)
-    else :
-        print("* " * i)
-        i -= 1
-print("* " * n)
+a1,a2 = n,1
+for i in range(2*n):
+    if i % 2 == 0:
+        print('* '*a1,end='')
+        a1 -= 1
+    else:
+        print('* '*a2,end='')
+        a2 += 1
+    print()
