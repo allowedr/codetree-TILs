ea21219e8887dd5a5c6b6c71b0851106a1078bc9
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-02 21:37:48 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241002/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+A = input()
+if A = 'S' :
+    print('Superior')
+elif A == 'A' :
+    print('Excellent')
+elif n == 'B' :
+    print('Good')
+elif n== 'C' :
+    print('Usually')
+elif n== 'D':
+    print('Effort')
+else :
+    print('Failure')
