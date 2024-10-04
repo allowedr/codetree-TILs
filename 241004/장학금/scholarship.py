1e8b2c4d9665669fc56588444b3163a7ec1d3389
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-04 20:59:43 +0900

    Title: 장학금, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/장학금/scholarship.py
+++ 241004/장학금/scholarship.py
@@ -2,7 +2,7 @@ m, l = map(int, input().split())
 if m >= 90 :
     if l >= 95 :
         print('100000')
-    elif m >= 90 :
+    elif l >= 90 :
         print('50000')
     else :
         print('0')
