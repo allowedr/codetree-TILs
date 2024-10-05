c66e1a58a434fbfe46133d2e8adb2e4073f2cf68
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-05 21:15:58 +0900

    Title: 코로나 메뉴얼, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,22 @@
+p = []
+for j in range(3) :
+    p.append(list(input().split()))
+a = 0
+b = 0
+c = 0
+d = 0
+
+for i in range(len(p)) :
+    if p[i][0] == 'Y' and int(p[i][1]) >= 37 :
+        a += 1
+    elif p[i][0] == 'N' and int(p[i][1]) >= 37 :
+        b += 1
+    elif p[i][0] == 'N' and int(p[i][1]) < 37 :
+        c += 1
+    else :
+        d += 1
+
+if a >= 2 :
+    print('E')
+else :
+    print('Y')
