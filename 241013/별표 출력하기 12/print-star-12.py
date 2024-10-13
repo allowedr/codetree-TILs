2cb3968c61712985def1595fb1d8b6b256fd1776
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-13 18:36:36 +0900

    Title: 별표 출력하기 12, Time: 212ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241013/별표 출력하기 12/print-star-12.py
+++ 241013/별표 출력하기 12/print-star-12.py
@@ -1,6 +1,10 @@
 n = int(input())
 for i in range(n, -1, -1) :
     for j in range(i) :
-        if j % 2 == 0:
+        if i == n :
             print('* '*i, end='')
-        print()
+        if i % 2 == 0 :
+            print('* ', end='')
+        else :
+            print(' ', end='')
+    print()
