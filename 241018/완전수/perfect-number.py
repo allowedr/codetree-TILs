d92eb870e712b088d80c73e7820fb18c93d1e2ae
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-18 14:33:16 +0900

    Title: 완전수, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241018/완전수/perfect-number.py
@@ -0,0 +1,10 @@
+start, end = map(int, input().split())
+cnt = 0
+for i in range(start, end):
+    tmp = 0
+    for j in range(1, i):
+        if i % j == 0 :
+            tmp += j
+    if tmp == i :
+        cnt += 1
+print(cnt)
