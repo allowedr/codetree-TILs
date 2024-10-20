5e5713e61c218373a52c9d3f12d6f6bdc8ce5a46
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-20 20:49:28 +0900

    Title: 배열 채우기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/배열 채우기/filling-array.py
+++ 241020/배열 채우기/filling-array.py
@@ -4,5 +4,7 @@ for i in range(len(arr)):
         for j in range(arr.index(arr[i])-1, -1, -1):
             print(arr[j], end=' ') 
         break
-        for j in range(len(arr), -1, -1):
-           print(arr[j], end=' ')
+    if arr.count(0) == 0 :
+        for j in range(len(arr)-1, -1, -1):
+            print(arr[j], end=' ') 
+        break
