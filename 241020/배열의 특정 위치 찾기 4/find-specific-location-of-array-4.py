3010a1da48beafb88d70c2782fe93fd59d7857db
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-20 21:00:23 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241020/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
+++ 241020/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -2,6 +2,12 @@ arr = list(map(int, input().split()))
 cnt = 0
 s = 0
 for i in range(len(arr)):
+    if arr[i] == 0 :
+        for j in range(i):
+            if arr[i] % 2 == 0 and arr[i] != 0 and arr[i] != 1:
+                cnt += 1
+                s += arr[i]    
+            break
     if arr[i] % 2 == 0 and arr[i] != 0 and arr[i] != 1:
         cnt += 1
         s += arr[i]
