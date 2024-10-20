6cc9142993fb118c6535008edef4a30e60a49ea8
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-20 20:54:46 +0900

    Title: 10개 입력받기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/10개 입력받기/receive-10-inputs.py
+++ 241020/10개 입력받기/receive-10-inputs.py
@@ -1,13 +1,13 @@
 arr = list(map(int, input().split()))
 s = 0
-for i in range(10):
+for i in range(len(arr)):
     if arr[i] == 0 :
         for j in range(i) :
             s += arr[j]
         print(f'{s} {s/i:.1f}')
         break
     if arr.count(0) == 0 :
-        for j in range(10) :
+        for j in range(len(arr)) :
             s += arr[j]
-        print(f'{s} {s/i:.1f}')
+        print(f'{s} {s/len(arr):.1f}')
         break
