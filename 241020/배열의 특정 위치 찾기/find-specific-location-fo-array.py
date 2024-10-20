be923c5975fb1c68f0d4ea2d940b8b9366d3e065
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-20 21:22:01 +0900

    Title: 배열의 특정 위치 찾기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 241020/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -7,7 +7,7 @@ cnt = 0
 for i in range(n) :
     if i % 2 == 1 :
         sum_a += arr[i]
-    if i-1 % 3 != 0 :
+    if i % 3 == 0 and i != 0 and i != 1:
         sum_b += arr[i-1]
         cnt += 1
 print(f'{sum_a} {sum_b/cnt:.1f}')
