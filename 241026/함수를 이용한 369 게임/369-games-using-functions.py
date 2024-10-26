183126376aa6b8bdd5517f1671a393e620757fe7
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-26 20:04:56 +0900

    Title: 함수를 이용한 369 게임, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241026/함수를 이용한 369 게임/369-games-using-functions.py
+++ 241026/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,7 +1,7 @@
 def samuku(a, b):
     cnt = 0
     for i in range(a, b+1):
-        if i == 3 or i == 6 or i == 9 or i%10 == 3 or i%10 == 6 or i%10 == 9 or i//10 == 3 or i//10 == 6 or i//10 == 9 or i % 3 == 0:
+        if i in [3, 6, 9]or i%10 in [3,6,9] or i//10 in [3,6,9] or i % 3 == 0:
             cnt += 1
     return cnt
 
