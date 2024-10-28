e72ef4b7a578f8ee4c0499c7dd6973bafd88f4f8
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-28 19:18:02 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 241028/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,9 +1,8 @@
 N = int(input())
 arr = list(map(int, input().split()))
 while True:
-    print(arr.index(max(arr))+1, end=' ')
-    arr = arr[:arr.index(max(arr))].copy()
-
     if arr.index(max(arr)) == 0 :
         print(arr.index(max(arr))+1, end=' ')
         break
+    print(arr.index(max(arr))+1, end=' ')
+    arr = arr[:arr.index(max(arr))].copy()
