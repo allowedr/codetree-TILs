6afe48a4dd072366b1a899969c74f97a5c94166f
allowedr <83686606+allowedr@users.noreply.github.com>
2024-10-30 17:26:35 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241030/배열 놀이/play-with-array.py
+++ 241030/배열 놀이/play-with-array.py
@@ -1,6 +1,6 @@
 n, p = map(int, input().split())
 arr = list(map(int, input().split()))
-for i in range(n):
+for i in range(n+1):
     cnt = 0
     question = list(map(int, input().split()))
     if len(question) == 3 :
