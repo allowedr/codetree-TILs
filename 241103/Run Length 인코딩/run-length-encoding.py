3a2898d7d49ded5c36a3b53825266dd19d10d5bf
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-03 18:41:13 +0900

    Title: Run Length 인코딩, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241103/Run Length 인코딩/run-length-encoding.py
+++ 241103/Run Length 인코딩/run-length-encoding.py
@@ -5,21 +5,21 @@ lcnt = 0
 if len(sen) == 1 :
     print(2)
     print(f"{sen}1")
-    break
-for i in range(1, len(sen)):
-    if sen[i] == sen[i-1]:
-        cnt += 1
-    else :
-        nsen.append(sen[i-1])
-        nsen.append(cnt)
-        lcnt += 2
-        cnt = 1
-    
-    if i == len(sen)-1:
-        nsen.append(sen[i])
-        nsen.append(cnt)
-        lcnt += 2
+else :
+    for i in range(1, len(sen)):
+        if sen[i] == sen[i-1]:
+            cnt += 1
+        else :
+            nsen.append(sen[i-1])
+            nsen.append(cnt)
+            lcnt += 2
+            cnt = 1
+        
+        if i == len(sen)-1:
+            nsen.append(sen[i])
+            nsen.append(cnt)
+            lcnt += 2
 
-print(lcnt)        
-for i in range(len(nsen)):
-    print(nsen[i], end='')
+    print(lcnt)        
+    for i in range(len(nsen)):
+        print(nsen[i], end='')
