ea5b20d3ec12c790d4592a53f246b3543769f52b
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-03 18:48:13 +0900

    Title: Run Length 인코딩, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241103/Run Length 인코딩/run-length-encoding.py
+++ 241103/Run Length 인코딩/run-length-encoding.py
@@ -1,25 +1,29 @@
-sen = input()
-nsen = []
-cnt = 1
-lcnt = 0
-if len(sen) == 1 :
-    print(2)
-    print(f"{sen}1")
-else :
-    for i in range(1, len(sen)):
-        if sen[i] == sen[i-1]:
-            cnt += 1
-        else :
-            nsen.append(sen[i-1])
-            nsen.append(cnt)
-            lcnt += 2
-            cnt = 1
-        
-        if i == len(sen)-1:
-            nsen.append(sen[i])
-            nsen.append(cnt)
-            lcnt += 2
+# 변수 선언 및 입력:
+A = input()
 
-    print(len(nsen))        
-    for i in range(len(nsen)):
-        print(nsen[i], end='')
+# 변환
+encoded = ""
+
+# 입력의 첫번째 값을 읽고 초기화합니다.
+curr_char = A[0]
+num_char = 1
+for traget_char in A[1:]:
+    if traget_char == curr_char:
+        # 지금까지의 문자와 같으면 연속된 문자 개수만 추가해 주고 넘어갑니다.
+        num_char += 1
+    else:
+        # 지금까지의 문자와 다르면 새로운 문자로 바꿔줘야 합니다.
+        # 지금까지 세어온 curr_char와 num_char를 기록합니다.
+        encoded += curr_char
+        encoded += str(num_char)
+
+        # curr_char와 num_char를 현재 값으로 초기화합니다.
+        curr_char = traget_char
+        num_char = 1
+    
+# 마지막 덩어리에 해당하는 curr_char와 num_char를 기록합니다.
+encoded += curr_char
+encoded += str(num_char)
+
+print(len(encoded))
+print(encoded)
