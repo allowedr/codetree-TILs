ab176ddc4bf8bc9286738e5da1903759daad423b
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-04 21:23:44 +0900

    Title: 첫 번째와 두 번째 교환, Time: 40ms, Memory: 15MB, Status: Passed - CodeTree

--- 241104/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 241104/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -7,7 +7,7 @@ arr[0], arr[1] = arr[1], arr[0]
 for i in range(2, len(arr)):
     if arr[i] == arr[1] :
         arr[i] = arr[0]
-    if arr[i] == arr[0] :
+    elif arr[i] == arr[0] :
         arr[i] = arr[1]
 
 
