2a8273a6c3605f532b1dc466ac6e746ebf5496c8
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-04 21:31:59 +0900

    Title: 문자열 놀이, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241104/문자열 놀이/play-with-string.py
+++ 241104/문자열 놀이/play-with-string.py
@@ -2,7 +2,7 @@ s, q = input().split(' ')
 sen = list(s)
 q = int(q)
 for i in range(q):
-    arr = list(input().split())
+    arr = list(input().split(' '))
     if arr[0] == '1' :
         sen[int(arr[1])-1], sen[int(arr[2])-1] = sen[int(arr[2])-1], sen[int(arr[1])-1]
         s = ''.join(sen)
