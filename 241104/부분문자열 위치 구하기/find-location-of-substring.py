7ae4866fb0464539d901d7db412b7d8a9bc697a5
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-04 20:50:04 +0900

    Title: 부분문자열 위치 구하기, Time: 40ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/부분문자열 위치 구하기/find-location-of-substring.py
+++ 241104/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,7 +1,11 @@
 s = input()
 target = input()
 
+exists = False
 for i in range(len(s)):
     if s[i:i + len(target)] == target :
         print(i)
-        break
+        break     
+
+if exists == False :
+    print('-1')
