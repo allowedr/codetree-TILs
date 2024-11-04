f0c8658333369668459fcdd8f09102e17dedb98a
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-04 21:23:23 +0900

    Title: 첫 번째와 두 번째 교환, Time: 40ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 241104/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -7,6 +7,8 @@ arr[0], arr[1] = arr[1], arr[0]
 for i in range(2, len(arr)):
     if arr[i] == arr[1] :
         arr[i] = arr[0]
+    if arr[i] == arr[0] :
+        arr[i] = arr[1]
 
 
 s = ''.join(arr)
