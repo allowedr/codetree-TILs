6b3cd834cf2ad0195f3e05a40ad2f5a9ff0741c0
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-05 19:24:52 +0900

    Title: 문자 제거하기, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241105/문자 제거하기/del-char.py
@@ -0,0 +1,13 @@
+s = input()
+
+while True :
+    arr = list(s)
+    a = int(input())
+    if a > len(arr):
+        arr.pop(len(arr)-1)
+        s = ''.join(arr)
+        print(s)
+        break
+    arr.pop(a)
+    s = ''.join(arr)
+    print(s)
