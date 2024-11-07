d56227530eec8ab7535044f08542e30eadea0dde
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-07 23:24:46 +0900

    Title: 짝수홀수 개수 세기, Time: 44ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241107/짝수홀수 개수 세기/count-even-odd-numbers.py
+++ 241107/짝수홀수 개수 세기/count-even-odd-numbers.py
@@ -1,10 +1,9 @@
 n = int(input())
 odd = 0
 even = 0
-for i in range(1,n-1):
+for i in range(n-1):
     c = int(input())
     if c == 0 :
-        odd += 1
         break
     elif c%2 != 0 :
         even += 1
