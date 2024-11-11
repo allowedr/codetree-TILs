7f264f3cd28a517eb4fd9749124e7d06d16800e8
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-11 20:46:10 +0900

    Title: k번째로 신기한 문자열, Time: 41ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241111/k번째로 신기한 문자열/kth-special-string.py
+++ 241111/k번째로 신기한 문자열/kth-special-string.py
@@ -2,9 +2,13 @@ n, k , T = input().split()
 n = int(n)
 k = int(k)
 arr = []
+ary = []
 for _ in range(n):
     arr.append(input())
 print(*arr)
 
-arr.sort()
-print(*arr)
+for i in range(n):
+    if arr[i][:2] == T :
+        ary.append(arr[i])
+ary.sort()
+print(*ary)
