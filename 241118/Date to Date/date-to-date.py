820c28eb6a90c048833775eef064ff3e441f1060
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-18 21:38:42 +0900

    Title: Date to Date, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 241118/Date to Date/date-to-date.py
+++ 241118/Date to Date/date-to-date.py
@@ -1,5 +1,5 @@
 amonth, aday, bmonth, bday = map(int, input().split())
-elapsed_days = 0
+elapsed_days = 1
 
 #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
