52e1bb6961d851ed2a9aec108fcdcdff226c7ee2
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-18 21:44:22 +0900

    Title: DateTime to DateTime, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241118/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,26 @@
+amonth, aday, ahour, amins, bmonth= 11, 11, 11, 11, 11
+bday, bhour, bmins  = map(int, input().split())
+elapsed_days = 0
+elapsed_time = 0
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+while True:
+    if amonth == bmonth and aday == bday and ahour == bhour and amins == bmins:
+        break
+    elapsed_time += 1
+    amins += 1
+    if aday > num_of_days[amonth]:
+        amonth += 1
+        aday = 1
+
+    if ahour == 24:
+        aday += 1
+        ahour = 0
+
+    if amins == 60:
+        ahour += 1
+        amins = 0
+
+print(elapsed_time)
