5e19be6f7dc8bf634935b5a6d24e25cdaa4f7e7a
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-18 21:53:02 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241118/DateTime to DateTime/datetime-to-datetime.py
+++ 241118/DateTime to DateTime/datetime-to-datetime.py
@@ -7,13 +7,13 @@ elapsed_time = 0
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 while True:
-    if bday < 11 :
-        if bhour < 11 :
-            if bmins < 11 :
-                print('-1')
-                break
-            print('-1')
-            break
+    if bday <= 11 :
+        print('-1')
+        break
+    if bday <= 11 and bhour < 11 :
+        print('-1')
+        break
+    if bday <= 11 and bhour <= 11 and bmin < 11 :
         print('-1')
         break
     if amonth == bmonth and aday == bday and ahour == bhour and amins == bmins:
