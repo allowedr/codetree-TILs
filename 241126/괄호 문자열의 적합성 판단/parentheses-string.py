37bb6bbdb1c462b5acf97252fdabce90773a5ae9
allowedr <83686606+allowedr@users.noreply.github.com>
2024-11-26 18:57:48 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 241126/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,15 +1,46 @@
-#문제 4번
-test = input()
+import sys
+
+class Stack:
+    def __init__(self):          # 빈 스택 하나를 생성합니다.
+        self.items = []
+                
+    def push(self, item):        # 스택에 데이터를 추가합니다.
+        self.items.append(item)
+                
+    def empty(self):             # 스택이 비어있으면 True를 반환합니다.
+        return not self.items
+                
+    def size(self):              # 스택에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 스택의 가장 위에 있는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+            
+        return self.items.pop()
+                
+    def top(self):               # 스택의 가장 위에 있는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+                        
+        return self.items[-1]
 
 
-left_count = 0#괄호 열기 카운트
-right_count = 0#괄호 닫기 카운드
-for gwalho in test:
-    if gwalho == '(':
-        left_count += 1
+# 변수 선언 및 입력:
+string = input()
+s = Stack()
+
+for char in string:
+    if char == '(':
+        s.push('(')
     else:
-        right_count += 1
-if left_count == right_count:# 괄호 열기와 닫기의 갯수가 맞다면 vps라고 판단
-    print('Yes')
+        if s.empty():
+            print("No")
+            sys.exit(0)
+
+        s.pop()
+
+if s.empty():
+    print("Yes")
 else:
-    print('No')
+    print("No")
