41224cf10db16a04adb281fbf3fbae43ef73ef86
Hanseong <83686606+allowedr@users.noreply.github.com>
2024-12-04 23:55:47 +0900

    Title: 함수를 이용한 윤년 판별, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241204/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,11 @@
+def is_onjeonsu(n):
+    if n % 2 == 0:
+        return False
+    if n % 10 == 5:
+        return False
+    if n % 3 == 0 and n % 9 != 0:
+        return False
+    return 'true'
+
+
+print(is_onjeonsu(9))
