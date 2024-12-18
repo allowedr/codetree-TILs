c38b0d98d5c576293c4622c09cf756b561cd1c11
Hanseong <83686606+allowedr@users.noreply.github.com>
2024-12-18 19:38:30 +0900

    Title: 최고의 33위치, Time: 93ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241218/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,34 @@
+
+n = int(input())
+grid = [
+	list(map(int, input().split())) 
+	for _ in range(n)
+	]
+
+
+
+
+def get_num_of_gold(row, col_s, col_e):
+	num_of_gold = 0
+
+	for row in range(row_S, row_e, 1):
+		for col in range(col_s, col_e + 1):
+			num_of_gold += grid[row][col]
+	
+	return num_of_gold
+
+max_gold = 0
+
+
+for row in range(n):
+	for col in range(n):
+
+		if col + 2 >= n or row + 2 >= n:
+			continue
+
+		num_of_gold = get_num_of_gold(row, col, col + 2)
+
+		
+		max_gold = max(max_gold, num_of_gold)
+
+print(max_gold)
